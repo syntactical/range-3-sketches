d499d9f16690bb33868c7215b0d87c2c224afb84
syntactical <greg.dutcher@gmail.com>
2016-04-06 18:22:26 -0400

    working with all 8 tentacles

--- installation/installation.ino
+++ installation/installation.ino
@@ -1,10 +1,12 @@
 #define LENGTH_OF_TAIL 4
-#define LINKS 6
+#define LINKS_PER_STRIP 6
 #define LEDS_PER_LINK 150
 
 #include <OctoWS2811.h>
 
-const int ledsPerStrip = LINKS * LEDS_PER_LINK;
+const int ledsPerStrip = LINKS_PER_STRIP * LEDS_PER_LINK;
+const int totalLinks = LINKS_PER_STRIP * 8;
+const int totalLeds = totalLinks * LEDS_PER_LINK;
 
 DMAMEM int displayMemory[ledsPerStrip*6];
 int drawingMemory[ledsPerStrip*6];
@@ -14,7 +16,7 @@ const int config = WS2811_GRB | WS2811_800kHz;
 OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, config);
 
 int analogPin = A9;
-int brightestPixels[LINKS];
+int brightestPixels[totalLinks];
 float attenuations[LENGTH_OF_TAIL+1];
 
 unsigned long previousMillis = 0;
@@ -25,8 +27,7 @@ float maxDelay;
 
 int pixelsPerStrip;
 
-void setup()
-{                
+void setup()  {                
   Serial.begin(38400);
 
   minDelay = 2;
@@ -43,7 +44,7 @@ void setup()
     attenuations[i] = scale;
   }
 
-  for (int link = 1; link < LINKS; link++) {
+  for (int link = 1; link < totalLinks; link++) {
     brightestPixels[link] = brightestPixels[link-1] + LEDS_PER_LINK;
   }
 }
@@ -53,22 +54,21 @@ void loop()  {
   long delayTime = long(clamp((20000/clamp(sensorValue-50,1,1023)/3), minDelay, maxDelay));
 
   unsigned long currentMillis = millis();
-
   
   if (currentMillis - previousMillis >= delayTime) {
     Serial.println(currentMillis - previousMillis);
     previousMillis = currentMillis;
 
-    for (int link = 0; link < LINKS; link++) {
+    for (int link = 0; link < totalLinks; link++) {
       for (int i=0; i<=LENGTH_OF_TAIL; i++)  {
-        int target = (LEDS_PER_LINK + brightestPixels[link] - i) % ledsPerStrip;
+        int target = (LEDS_PER_LINK + brightestPixels[link] - i) % totalLeds;
         leds.setPixel(target, leds.color(attenuations[i], attenuations[i], attenuations[i]));
       }
-   
-      leds.show();
-      
-      brightestPixels[link] = (brightestPixels[link] + 1) % ledsPerStrip;
+
+      brightestPixels[link] = (brightestPixels[link] + 1) % totalLeds;
     }
+
+    leds.show();
   }
 }
 
