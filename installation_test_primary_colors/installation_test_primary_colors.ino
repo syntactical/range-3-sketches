219f53771920e2f3dd8cedd21b1c80a3d2cf7689
syntactical <greg.dutcher@gmail.com>
2016-04-09 18:13:38 -0400

    working wiht 3600 LEDs

--- installation_test_primary_colors/installation_test_primary_colors.ino
+++ installation_test_primary_colors/installation_test_primary_colors.ino
@@ -1,11 +1,12 @@
 #include <OctoWS2811.h>
 
-const int red = 0xFF0000;
-const int yellow = 0xFFFF00;
-const int blue = 0x0000FF;
+const int red = 0x110000;
+const int yellow = 0xddddff;
+const int blue = 0x000000;
 
+const int stripLength = 150;
 const int ledsPerStrip = 900;
-const int totalLeds = 150;
+const int totalLeds = 3600;
 
 int colorcycle[] = {red, yellow, blue};
 
@@ -26,10 +27,11 @@ void setup()  {
 
 void loop()  {
   float sensorValue = 50 * (sin(millis() / 2000.0f) + 1);
+//  float sensorValue = analogRead(A9)/10.0f;
   
   for (int i=0; i<=totalLeds; i++)  {
-    int position = i / (totalLeds / (sensorValue));
-    leds.setPixel(i, colorcycle[(position % 3)]);
+    int position = i / (totalLeds / (sensorValue)) - 50;
+    leds.setPixel(transpose(i), colorcycle[(position % 3)]);
   }
 
   leds.show();
@@ -39,4 +41,19 @@ void loop()  {
 
 inline float clamp(float x, float a, float b){
     return x < a ? a : (x > b ? b : x);
+}
+
+inline int transpose(int position){
+  int transposedPosition;
+  
+  bool reversed = (position / stripLength) % 2 == 1;
+
+  if (reversed) {
+    int stripStartPosition = ((position / stripLength) * stripLength);
+    transposedPosition = stripStartPosition + ((stripLength - 1) - (position - stripStartPosition));
+  } else {
+    transposedPosition = position;
+  }
+
+  return transposedPosition;
 }
